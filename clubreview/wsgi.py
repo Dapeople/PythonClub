8e2912a9366dbb3ed58b2fce0cdd996e7371a4d3
Carl Vannorman <dapeople1@gmail.com>
2022-01-23 19:29:37 -0800

    Variable name change

--- clubreview/wsgi.py
+++ clubreview/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'techreview.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'clubreview.settings')
 
 application = get_wsgi_application()
